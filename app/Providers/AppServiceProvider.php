647b02aae8f171e34d3b3245386ba55330eeacd9
Ahmet Rasim Beyhan <ahmetrasimbayhan@gmail.com>
2025-11-03 18:35:59 +0300

    issue fixed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 use Carbon\Carbon;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,5 +24,15 @@ class AppServiceProvider extends ServiceProvider
         // Set default timezone for Carbon (Istanbul/Turkey GMT+3)
         Carbon::setLocale('tr'); // Turkish locale
         date_default_timezone_set('Europe/Istanbul');
+
+        // Force HTTPS URLs in production to prevent mixed content
+        if (app()->environment('production')) {
+            $appUrl = config('app.url');
+            $scheme = parse_url($appUrl, PHP_URL_SCHEME) ?: 'http';
+            if ($scheme === 'https') {
+                URL::forceRootUrl($appUrl);
+                URL::forceScheme('https');
+            }
+        }
     }
 }
